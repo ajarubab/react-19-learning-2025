1c61048427d9b3208165d1e92513af59eef8d0ad
Raja kumar prasad <ajarubab@gmail.com>
2025-03-02 18:09:29 +0530

    creating a new react component and executing some functions on it

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import User from './user'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Lets learn react 19 also with laravel 11 and vue3</h1>
+      <h1>Count Value : {count}</h1>
+      <button onClick={()=>setCount(count+1)}> + </button>
+      <User/>
     </>
   )
 }
