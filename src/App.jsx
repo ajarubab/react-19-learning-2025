e6542eb16dfc0dc9e4a19e633e335892f4aed0e6
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 16:16:29 +0530

    homework vd 10 csbs

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import { createElement } from "react";
 function App() {
-    return createElement(
-        "div",
-        { id: "rootDiv" },
-        "Hello Div",
-        createElement(
-            "div",
-            { id: "mainDiv" },
-            createElement(
-                "h1",
-                { id: "h1Tag" },
-                "Jai Siya Ram"
-            )
-        ),
-        createElement(
-            "div",
-            { id: "rootDiv" },
-            createElement(
-                "button",
-                { class: "btn1", onClick: () => alert('Radhe Radhe') },
-                "Click me"
-            )
-        )
-    )
+    function callFun() { alert("Function called"); }
+    return (
+        <div>
+            <h1>Raja Kumar prasad</h1>
+            <img class="photo" src="https://www.euroschoolindia.com/wp-content/uploads/2023/09/how-sun-powers-our-planet.jpg" alt="#" />
+            <ul>
+                <li>A for Apple</li>
+                <li>A for Aata</li>
+                <li>A for Aam</li>
+            </ul>
+            {/* <button onClick={()=>alert("function called")}>Click me</button> */}
+            <button onClick={callFun}>Click me</button>
+        </div>
+    );
 }
 
 export default App;
