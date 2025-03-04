c81765252a448019cea4ee693c297164cd037adf
Raja kumar prasad <ajarubab@gmail.com>
2025-03-04 15:23:53 +0530

    basic operations of JSX file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import Home, {Color, Fruit, result} from "./Home";
+import Home, { Color, Fruit, result } from "./Home";
 
-function App(){
+function App() {
+    const Name = "Raja kumar Prasad";
+    let x = 12;
+    let y = 25;
     return (
         <div>
             <h1>App Component</h1>
             <Home />
-            <Color/>
+            <Color />
             <Fruit />
             <h1>Sum : {result}</h1>
+            <h2>{12 + 23 + 34 + 45}</h2>
+            <h1>Name is : {Name}</h1>
+            <h1>{x} * {y} = {x * y}</h1>
+            <button onClick={()=>alert("Hello")}>Click me</button>
         </div>
+
     );
 }
 
